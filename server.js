ccca167e5094835c8db402cef199b1d6ed4559a5
manos <bmanos@users.noreply.github.com>
2021-02-05 09:42:51 +0200

    fix

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
 // Create the express app
-const express = require("express")
+const express = require('express')
 const app = express()
-const db = require("./database.js") // Link the database.js file
-const md5 = require("md5")
-const path = require("path");
+const db = require('./database.js') // Link the database.js file
+const md5 = require('md5')
+//const path = require('path');
 
 // Use bodyparser middleware to POST data
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // set the view engine to ejs
 // Use express to get static folder routing
 app.use(express.static('public'));
-// app.set("view engine", "ejs");
-// app.set("views", path.join(__dirname, "views"));
-// app.use(express.static(path.join(__dirname, "public")));
+// app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.urlencoded({ extended: false }));
 
 // Define server port number
 const HTTP_PORT = 3000 
 // Start the server
 app.listen(HTTP_PORT, () => {
-    console.log("Server is running on port %PORT%".replace("%PORT%",HTTP_PORT))
+    console.log('Server is running on port %PORT%'.replace('%PORT%',HTTP_PORT))
 });
 // Routing to the / with GET and get message response of json format
-app.get("/", (req, res, next) => {
-    res.json({"message":"App is up and running"})
+app.get('/', (req, res, next) => {
+    res.json({'message':'App is up and running'})
     //res.render('index')
 });
 
@@ -44,50 +44,68 @@ thanks to: https://developerhowto.com/2018/12/29/build-a-rest-api-with-node-js-a
 */
 
 // API endpoints
-// Get a list of entities GET method, all users (/api/users/)
-app.get("/api/users", (req, res, next) => {
-    let sql = "select * from user"
+
+
+app.get('/myApi/Allusers', (req, res) => {
+    let sql = 'select * from user'
     let params = []
     db.all(sql, params, (err, rows) => {
         if (err) {
-          res.status(400).json({"error":err.message});
+          res.status(400).json({'error':err.message});
           return;
         }
          res.json({
              //"message":"success",
-             "data":rows
+             'data':rows
          })
-        //res.render("users", {model:rows});
+        //res.render('users', {model:rows});
+      });
+});
+
+// Get a list of entities GET method, all users (/api/users/)
+app.get('/api/users', (req, res, next) => {
+    let sql = 'select * from user'
+    let params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({'error':err.message});
+          return;
+        }
+         res.json({
+             //"message":"success",
+             'data':rows
+         })
+        //res.render('users', {model:rows});
       });
 });
 
 // Get a single entity by id GET method, a user by id (/api/user/{id})
-app.get("/api/user/:id", (req, res, next) => {
-    let sql = "select * from user where id = ?"
+app.get('/api/user/:id', (req, res, next) => {
+    let sql = 'select * from user where id = ?'
     let params = [req.params.id]
     db.get(sql, params, (err, row) => {
         if (err) {
-          res.status(400).json({"error":err.message});
+          res.status(400).json({'error':err.message});
           return;
         }
         res.json({
-            "message":"success",
-            "data":row
+            'message':'success',
+            'data':row
         })
       });
 });
 
 // Create entity POST method, create new user (/api/user/}
-app.post("/api/user/", (req, res, next) => {
+app.post('/api/user/', (req, res, next) => {
     let errors=[]
     if (!req.body.password){
-        errors.push("No password specified");
+        errors.push('No password specified');
     }
     if (!req.body.email){
-        errors.push("No email specified");
+        errors.push('No email specified');
     }
     if (errors.length){
-        res.status(400).json({"error":errors.join(",")});
+        res.status(400).json({'error':errors.join(',')});
         return;
     }
     let data = {
@@ -99,19 +117,19 @@ app.post("/api/user/", (req, res, next) => {
     let params =[data.name, data.email, data.password]
     db.run(sql, params, function (err, result) {
         if (err){
-            res.status(400).json({"error": err.message})
+            res.status(400).json({'error': err.message})
             return;
         }
         res.json({
-            "message": "success",
-            "data": data,
-            "id" : this.lastID
+            'message': 'success',
+            'data': data,
+            'id' : this.lastID
         })
     });
 })
 
 // Update entity by id PATCH method, update user(/api/user/{id})
-app.patch("/api/user/:id", (req, res, next) => {
+app.patch('/api/user/:id', (req, res, next) => {
     let data = {
         name: req.body.name,
         email: req.body.email,
@@ -126,11 +144,11 @@ app.patch("/api/user/:id", (req, res, next) => {
         [data.name, data.email, data.password, req.params.id],
         function (err, result) {
             if (err){
-                res.status(400).json({"error": res.message})
+                res.status(400).json({'error': res.message})
                 return;
             }
             res.json({
-                message: "success",
+                message: 'success',
                 data: data,
                 changes: this.changes
             })
@@ -138,16 +156,16 @@ app.patch("/api/user/:id", (req, res, next) => {
 })
 
 // Delete entity by id DELETE method, a user (/api/user/{id})
-app.delete("/api/user/:id", (req, res, next) => {
+app.delete('/api/user/:id', (req, res, next) => {
     db.run(
         'DELETE FROM user WHERE id = ?',
         req.params.id,
         function (err, result) {
             if (err){
-                res.status(400).json({"error": res.message})
+                res.status(400).json({'error': res.message})
                 return;
             }
-            res.json({"message":"deleted", changes: this.changes})
+            res.json({'message':'deleted', changes: this.changes})
     });
 })
 
